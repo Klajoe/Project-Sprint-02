175841cdbec8130c661c1b8819188c74fae4625b
Klajoe <parlar_ar@hotmail.com>
2023-05-19 20:32:09 +0300

    Last commmit

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
 $(document).ready(function() {
     $("#datepicker").datepicker();
 
-    $('#submit').click(function() {
+    $('#submit').click(function(e) {
+        e.preventDefault();
+
         var username = $('#username').val();
         var moviename = $('#moviename').val();
         var comment = $('#comment').val();
         var date = $('#datepicker').val();
 
-        if (username !== '' && moviename !== '' && comment !== '') {
-            var commentHTML = '<div class="comment">' +
-                '<div class="username">' + username + '</div>' +
-                '<div class="moviename">' + moviename + '</div>' +
-                '<div class="date">' + date + '</div>' +
-                '<div class="content">' + comment + '</div>' +
-                '</div>';
-
-            $('.comments-container').prepend(commentHTML);
-
-            // İlgili alanları temizle
-            $('#username').val('');
-            $('#moviename').val('');
-            $('#comment').val('');
-            $('#datepicker').val('');
+        if (username === '' || moviename === '' || comment === '') {
+            alert('Please fill in all the required fields.');
+            return;
         }
+
+        var commentHTML = '<div class="comment">' +
+            '<div class="username">' + username + '</div>' +
+            '<div class="moviename">' + moviename + '</div>' +
+            '<div class="date">' + date + '</div>' +
+            '<div class="content">' + comment + '</div>' +
+            '</div>';
+
+        $('.comments-container').prepend(commentHTML);
+
+        // Clear the input fields
+        $('#username').val('');
+        $('#moviename').val('');
+        $('#comment').val('');
+        $('#datepicker').val('');
     });
 });
